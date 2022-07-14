c10ad4a6fdeeb5433bece256243fb0a6f5d7185d
Vid Mahovic <>
2022-07-14 15:53:17 +0200

    Implement auction handlers

--- src/nounlet-auction.ts
+++ src/nounlet-auction.ts
@@ -1,55 +1,106 @@
 import {
-    ActiveModules as ActiveModulesEvent,
     AuctionBid as AuctionBidEvent,
     AuctionCreated as AuctionCreatedEvent,
     AuctionExtended as AuctionExtendedEvent,
     AuctionSettled as AuctionSettledEvent,
 } from "../generated/NounletAuction/NounletAuction";
-import { ActiveModules, AuctionBid, AuctionCreated, AuctionExtended, AuctionSettled } from "../generated/schema";
-
-export function handleActiveModules(event: ActiveModulesEvent): void {
-    let entity = new ActiveModules(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-    entity._vault = event.params._vault;
-    entity._modules = event.params._modules;
-    entity.save();
-}
-
-export function handleAuctionBid(event: AuctionBidEvent): void {
-    let entity = new AuctionBid(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-    entity._vault = event.params._vault;
-    entity._token = event.params._token;
-    entity._id = event.params._id;
-    entity._sender = event.params._sender;
-    entity._value = event.params._value;
-    entity._extended = event.params._extended;
-    entity.save();
-}
+import { Auction, Bid, Nounlet, Seed, Vault } from "../generated/schema";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 
 export function handleAuctionCreated(event: AuctionCreatedEvent): void {
-    let entity = new AuctionCreated(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-    entity._vault = event.params._vault;
-    entity._token = event.params._token;
-    entity._id = event.params._id;
-    entity._startTime = event.params._startTime;
-    entity._endTime = event.params._endTime;
-    entity.save();
+    const vaultId: string = event.params._vault.toHexString();
+    const nounletId: string = event.params._id.toString();
+    const startTime: BigInt = event.params._startTime;
+    const endTime: BigInt = event.params._endTime;
+
+    const vault = Vault.load(vaultId);
+    if (vault === null) {
+        log.error("[handleAuctionCreated] Vault not found: {}. Hash: {}", [
+            vaultId,
+            event.transaction.hash.toHexString(),
+        ]);
+        return;
+    }
+
+    // Store the seed of the nounlet
+    const seed = new Seed(nounletId);
+    seed.save();
+    // Store the nounlet
+    const nounlet = new Nounlet(nounletId);
+    nounlet.noun = vault.noun;
+    nounlet.seed = seed.id;
+    nounlet.save();
+    // Store the auction
+    const auction = new Auction(nounletId);
+    auction.nounlet = nounlet.id;
+    auction.amount = BigInt.fromI32(0);
+    auction.settled = false;
+    auction.startTime = startTime;
+    auction.endTime = endTime;
+    auction.save();
 }
 
 export function handleAuctionExtended(event: AuctionExtendedEvent): void {
-    let entity = new AuctionExtended(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-    entity._endTime = event.params._endTime;
-    entity._vault = event.params._vault;
-    entity._token = event.params._token;
-    entity._id = event.params._id;
-    entity.save();
+    const auctionId = event.params._id.toString();
+    const endTime = event.params._endTime;
+
+    const auction = Auction.load(auctionId);
+    if (auction === null) {
+        log.error("[handleAuctionExtended] Auction not found for Nounlet #{}. Hash: {}", [
+            auctionId,
+            event.transaction.hash.toHexString(),
+        ]);
+        return;
+    }
+
+    auction.endTime = endTime;
+    auction.save();
+}
+
+export function handleAuctionBid(event: AuctionBidEvent): void {
+    const auctionId = event.params._id.toString();
+    const bidder = event.params._sender.toHexString();
+    const bidAmount = event.params._value;
+
+    const auction = Auction.load(auctionId);
+    if (auction === null) {
+        log.error("[handleAuctionBid] Auction not found for Nounlet #{}. Hash: {}", [
+            auctionId,
+            event.transaction.hash.toHexString(),
+        ]);
+        return;
+    }
+
+    auction.bidder = bidder;
+    auction.amount = bidAmount;
+    auction.save();
+
+    const bidId = event.transaction.hash.toHexString();
+    const bid = new Bid(bidId);
+    bid.auction = auction.id;
+    bid.bidder = bidder;
+    bid.amount = bidAmount;
+    bid.blockNumber = event.block.number;
+    bid.blockTimestamp = event.block.timestamp;
+    bid.save();
 }
 
 export function handleAuctionSettled(event: AuctionSettledEvent): void {
-    let entity = new AuctionSettled(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
-    entity._vault = event.params._vault;
-    entity._token = event.params._token;
-    entity._id = event.params._id;
-    entity._winner = event.params._winner;
-    entity._amount = event.params._amount;
-    entity.save();
+    const auctionId = event.params._id.toString();
+    const amount = event.params._amount;
+    const winnerAddress = event.params._winner.toHexString();
+
+    const auction = Auction.load(auctionId);
+    if (auction === null) {
+        log.error("[handleAuctionSettled] Auction not found for Nounlet #{}. Hash: {}", [
+            auctionId,
+            event.transaction.hash.toHexString(),
+        ]);
+        return;
+    }
+
+    auction.settled = true;
+    auction.amount = amount;
+    auction.bidder = winnerAddress;
+    auction.save();
 }
