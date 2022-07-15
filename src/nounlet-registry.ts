2173a43a0b903e4e1fd75183f74c4a73ff4b6ba7
Vid Mahovic <>
2022-07-15 14:47:50 +0200

    Update VaultDeployed handler

--- src/nounlet-registry.ts
+++ src/nounlet-registry.ts
@@ -1,11 +1,8 @@
-import { VaultDeployed as VaultDeployedEvent } from "../generated/NounletRegistry/NounletRegistry"
-import { VaultDeployed } from "../generated/schema"
+import { VaultDeployed as VaultDeployedEvent } from "../generated/NounletRegistry/NounletRegistry";
+import { Vault } from "../generated/schema";
 
 export function handleVaultDeployed(event: VaultDeployedEvent): void {
-  let entity = new VaultDeployed(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._vault = event.params._vault
-  entity._token = event.params._token
-  entity.save()
+    const vaultId = event.params._vault.toHexString();
+    const vault = new Vault(vaultId);
+    vault.save();
 }
