d443edef8242fa6fbee4f9c1baf9a7d51b8bed85
Vid Mahovic <>
2022-08-22 15:22:35 +0200

    Add support for dynamic contracts

--- src/nounlet-registry.ts
+++ src/nounlet-registry.ts
@@ -1,8 +1,10 @@
 import { VaultDeployed as VaultDeployedEvent } from "../generated/NounletRegistry/NounletRegistry";
 import { findOrNewVault } from "./utils/helpers";
+import { NounletToken } from "../generated/templates";
 
 export function handleVaultDeployed(event: VaultDeployedEvent): void {
     const vault = findOrNewVault(event.params._vault.toHexString());
     vault.tokenAddress = event.params._token.toHexString();
     vault.save();
+    NounletToken.create(event.params._token);
 }
